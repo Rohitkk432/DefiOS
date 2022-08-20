92d46cba7da38d52ed1ed0e723af59db686074e9
Rohitkk432 <74586376+Rohitkk432@users.noreply.github.com>
2022-08-21 00:26:15 +0530

    minor changes

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -11,21 +11,24 @@ const Homepage: React.FC<HomepageProps> = ({}) => {
         <div className='w-screen h-screen homepageGradient px-[6%] py-[2%] text-white flex flex-col justify-start items-center'>
             
             {/* backdrop globe */}
-            <div className='h-full w-[88%] absolute top-[0%] overflow-hidden z-index-1'>
+            <div className='h-full w-[88%] absolute top-[0%] overflow-hidden z-10'>
                 <img src="/assets/images/backdrop.png" className='h-full m-auto' />
             </div>
 
             {/* navbar */}
-            <div className='w-full flex flex-row justify-between items-center h-[7vh] z-index-2'>
+            <div className='w-full flex flex-row justify-between items-center h-[7vh] z-20'>
                 <img src="/assets/images/defi-os-logo.png" className='h-full' />
                 <div className='flex flex-row justify-end items-center w-full h-full' >
                     <div className='mx-[2%] font-thin text-[2.5vh]'>Whiteboard Explainer</div>
                     <div className='mx-[2%] font-thin text-[2.5vh]'>Whitepaper</div>
-                    <div className='mx-[2%] font-thin text-[2.5vh]'>Contact Us <FontAwesomeIcon icon={faSquareArrowUpRight}  /> </div>
+                    <div className='mx-[2%] font-thin text-[2.5vh] flex flex-row justify-center items-center'>
+                        <div>Contact Us</div>
+                        <img src="/assets/images/UpRightArrow.svg"  className='inline h-[3vh]'/>
+                    </div>
                 </div>
             </div>
 
-            <div className='h-full w-full flex justify-center items-center z-index-2'>
+            <div className='h-full w-full flex justify-center items-center z-20'>
                 <div className=''>
                     <div className="flex flex-row justify-center items-center leading-[12vh] text-[10vh] font-semibold workSansFont">
                         <div className='mx-[1%] gradientText1' >Making</div>
@@ -47,8 +50,14 @@ const Homepage: React.FC<HomepageProps> = ({}) => {
                         </div>
                     </div>
                     <div className='flex flex-row justify-center items-center w-full text-[#1D242D] text-[2vh] font-semibold'>
-                        <button className='bg-[#D1D2D2] py-[2.5%] w-[27%] rounded-[0.75vh] mx-[2%]' ><FontAwesomeIcon icon={faSquareArrowUpRight} className='mr-[2%]' /> Get Started</button>
-                        <button className='bg-[#7082C3] py-[2.5%] w-[27%] rounded-[0.75vh] mx-[2%]' ><FontAwesomeIcon icon={faSquareArrowUpRight} className='mr-[2%]' /> Latest Updates</button>
+                        <button className='bg-[#D1D2D2] py-[2.5%] w-[27%] rounded-[0.75vh] mx-[2%] flex flex-row justify-center items-center' >
+                            <img src="/assets/images/UpRightArrow2.svg"  className='inline h-[3vh]'/>
+                            <div>Get Started</div>
+                        </button>
+                        <button className='bg-[#7082C3] py-[2.5%] w-[27%] rounded-[0.75vh] mx-[2%] flex flex-row justify-center items-center' >
+                            <img src="/assets/images/UpRightArrow2.svg"  className='inline h-[3vh]'/>
+                            <div>Latest Updates</div>
+                        </button>
                     </div>
                 </div>
             </div>
